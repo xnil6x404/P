211fcd074e70ef23dbc21bd4eeb4244cb3ee94dc
Badboy-404 <108590422+badboysabbir8@users.noreply.github.com>
2024-12-21 16:53:06 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[💕 ENDY BOT 💕]";
+const doNotDelete = "[💕 XNIL BOT 💕]";
 /**
 * @author NTKhang
 * @author: do not delete it
